462df33cc1962a3dc9180f63a9b447daa9a7e61e
hemnsoftare <138198540+hemnsoftare@users.noreply.github.com>
2024-07-29 23:16:43 +0300

    create first template

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="text-3xl font-extrabold text-red-600">page hi bye</div>
+  );
 };
 
 export default page;
