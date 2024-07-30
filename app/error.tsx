b0ebc6a71f8cefb19923cf636d4e029d140506e2
hemnsoftare <138198540+hemnsoftare@users.noreply.github.com>
2024-07-30 22:35:42 +0300

    add error page

---
+++ app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+const ErrorComponent = ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  return (
+    <div className="w-full flex flex-col items-center justify-center">
+      <p className="dark:text-white m-6 ">Error: {error.message}</p>
+      <button
+        className="border-amber-400 border-4 py-2 px-6 transition-all duration-200 rounded-lg  hover:bg-amber-600 dark:text-white"
+        onClick={reset}
+      >
+        Try Again
+      </button>
+    </div>
+  );
+};
+
+export default ErrorComponent;
